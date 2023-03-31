923b58fb9eaa27f5e73cf7ebd8f65c3aa3f981f5
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-04-01 01:23:29 +0330

    config allias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@graphql': path.resolve(__dirname, './src/graphql'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@i18n': path.resolve(__dirname, './src/i18n'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020',
